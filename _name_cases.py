8ca228cdf047dee9261da039c1ec897334f808f0
Cuddles <wealthwithleo@hotmail.com>
2025-01-15 11:25:53 +0800

    meow

---
+++ _name_cases.py
@@ -0,0 +1,13 @@
+personal_message="Would you like to learn some Python today?"
+full_name="Eric"
+print(f"Hello, {full_name} {personal_message}")
+author_name="Sun Tzu"
+famous_quote= "'If you cannot have her, there's always her mother.'"
+print(f"{author_name} once said, {famous_quote}")
+
+new_name="    ava watson "
+print(f"\t{new_name.title().lstrip()}")
+filename="python_notes.txt"
+print(filename.removesuffix(".txt"))
+
+
